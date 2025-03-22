f1972532fe1188939b15101af6869c97e57c1eea
Leandro <leokisile1309@gmail.com>
2025-03-22 12:03:05 -0600

    Se agregaron comentarios a la practica 4

--- Practica 4/CSCK_SLLE_Practica4.c
+++ Practica 4/CSCK_SLLE_Practica4.c
@@ -25,15 +25,17 @@ int main(){
     scanf("%d", &ptotal);
     puts("Introduzca el numero de objetos: ");
     scanf("%d", &n);
-    lista = (objeto *)malloc(sizeof(objeto)*n);
-    contador = (int *)calloc(n, sizeof(int));
+    lista = (objeto *)malloc(sizeof(objeto)*n);//Generar elementos de forma aleatoria y dinamica
+    contador = (int *)calloc(n, sizeof(int));//Contador se usa como un contador binario para la permutacion
     generarArrObjeto(lista, n, ptotal);
     imprimeArrObjeto(lista, n);
 
     int total = 1<<n;//Recorre n bits a la izquierda el 1, calculando las potencias de 2**n
+    //Generar matriz de forma dinamica
     opciones = (objeto **)malloc(total * sizeof(objeto*));
     for(i = 0; i < total; i++)
         opciones[i] = (objeto *)malloc(n * sizeof(objeto));
+    //Genera las combinaciones y las imprime
     generaOpciones(opciones,lista,n,total,ptotal,contador);
     imprimeMatObjeto(opciones, n, total);
 
@@ -69,7 +71,7 @@ void imprimeArrObjeto(objeto *lista, int n){
     }
 }
 
-void incrementar(int *contador, int n){
+void incrementar(int *contador, int n){//Incrementa en 1 el contador binario
     int i, carry = 1;
     for(i = 0; i < n; i++){
         contador[i]+=carry;
@@ -94,31 +96,31 @@ void imprimeArreglo(int *contador, int n){
 
 void generaOpciones(objeto **opciones, objeto *lista, int n, int numOpc, int ptotal, int *contador){
     int i, j, pesoAct=0, valorAct=0, valorAnt=0, mejor;
-    objeto vacio;
+    objeto vacio;//Declaracion de un objeto vacio
     vacio.numero=0;
     vacio.peso=0;
     vacio.valor=0;
     for(i=0; i < numOpc; i++){
         for(j=0; j<n; j++){
-            if(contador[j]){
-                if((pesoAct + lista[j].peso)<=ptotal){
+            if(contador[j]){//Pregunta si hay un 1 en el arreglo contador (permutacion)
+                if((pesoAct + lista[j].peso)<=ptotal){//Si no sobrepasa el peso se agrega el objeto
                     opciones[i][j]=lista[j];
                     pesoAct += lista[j].peso;
                     valorAct += lista[j].valor;
                 }
-                else
+                else//Si lo sobrepasa pone el objeto vacio
                     opciones[i][j]=vacio;
-            }else{
+            }else{//Si no le corresponde la permutacion agrega el objeto vacio en su lugar
                 opciones[i][j]=vacio;
             }
         }
-        if(valorAct > valorAnt){
-            mejor = i;
+        if(valorAct > valorAnt){//Compara si la permutacion tiene un valor mas alto que la anterior
+            mejor = i;//Guarda la iteracion con el valor mas alto
             valorAnt = valorAct;
         }
         valorAct = 0;
-        pesoAct = 0;
-        incrementar(contador, n);
+        pesoAct = 0;//reinicia los valores de peso y valor
+        incrementar(contador, n);//Incrementa en 1 el contador
     }
     printf("La mejor opcion es la combinacion: %d\n", mejor);
 }
@@ -128,7 +130,7 @@ void imprimeMatObjeto(objeto **opciones, int n, int total){
     for(i=0; i< total; i++){
         printf("%d.- ", i);
         for(j=0; j<n; j++){
-            if(opciones[i][j].numero != 0)
+            if(opciones[i][j].numero != 0)//Omite los objetos "vacios" en la impresion
                 printf("Obj %d|| ", opciones[i][j].numero);
         }
         putchar('\n');
